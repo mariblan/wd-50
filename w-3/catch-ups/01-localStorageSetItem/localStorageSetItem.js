3bfff37ff674f2dfe8007c153032729cc9485896
mariblan <mariablancocanal@gmail.com>
2024-04-16 13:05:24 +0200

    Local storage activities are added to repo

---
+++ w-3/catch-ups/01-localStorageSetItem/localStorageSetItem.js
@@ -0,0 +1,20 @@
+// string array for step 2
+const strArray = ['Miriam', 'Blanca', 'Paul', 'Will', 'Alex'];
+
+// object array for step 3
+const objArray = [
+  {
+    name: 'Miriam',
+    age: 32,
+  },
+  {
+    name: 'Alex',
+    age: 29,
+  },
+];
+
+// Step 1 -> Add your code
+
+// Step2 -> Add your code
+
+// Step3 -> Add your code
---
+++ w-3/catch-ups/03-localStorageremoveItem/localStorageRemove.js
@@ -0,0 +1,24 @@
+// string for step 1
+const myName = 'Miriam';
+localStorage.setItem('myName', myName);
+// string array for step 2
+const strArray = ['Miriam', 'Blanca', 'Paul', 'Will', 'Alex'];
+localStorage.setItem('strArray', JSON.stringify(strArray));
+// object array for step 3
+const objArray = [
+  {
+    name: 'Miriam',
+    age: 32,
+  },
+  {
+    name: 'Alex',
+    age: 29,
+  },
+];
+localStorage.setItem('objArray', JSON.stringify(objArray));
+
+// Step 1 -> Add your code
+
+// Step2 -> Add your code
+
+// Step3 -> Add your code
