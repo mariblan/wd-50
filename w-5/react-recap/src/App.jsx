0612407018100ccbe3c38e1e5e5b81fbb6e4e6cb
mariblan <mariablancocanal@gmail.com>
2024-04-30 15:04:40 +0200

    Adding the recap react code

--- w-5/react-recap/src/App.jsx
+++ w-5/react-recap/src/App.jsx
@@ -2,17 +2,48 @@ import './App.css';
 import profile from './assets/profile.jpeg';
 import profile2 from './assets/profile2.jpeg';
 import Card from './components/Card';
+import { useState } from 'react';
 
 function App() {
-  const name = 'Maria';
-  const email = 'maria@gmail.com';
-  const students = ["Maria", "Ana"];
+  const [cards, setCards] = useState([
+    {
+      id: `${new Date()}maria`,
+      src: profile,
+      name: 'Maria',
+      email: 'maria@gmail.com',
+      active: true,
+      busy: false,
+    },
+    {
+      id: `${new Date()}ana`,
+      src: profile2,
+      name: 'Ana',
+      email: 'ana@gmail.com',
+      active: true,
+      busy: true,
+    },
+    {
+      id: `${new Date()}mario`,
+      src: profile2,
+      name: 'Mario',
+      email: 'Mario@gmail.com',
+      active: false,
+      busy: false,
+    },
+  ]);
+
   return (
-    <>
+    <div className='App'>
       <h1>Hello</h1>
-      <Card name='Ana' email='ana@gmail.com' src={profile} />
-      <Card name={name} email={email} src={profile2} />
-    </>
+      {cards.map((cardObject) => (
+        <Card
+          key={cardObject.id}
+          cards={cards}
+          setCards={setCards}
+          cardObject={cardObject}
+        />
+      ))}
+    </div>
   );
 }
 
--- w-5/react-recap/src/components/Card.jsx
+++ w-5/react-recap/src/components/Card.jsx
@@ -1,21 +1,50 @@
-import profile from '../assets/profile.jpeg';
+import { useState, useEffect } from 'react';
 
-function Card({src, name, email}) {
-    // console.log(props)
+function Card({
+  cardObject: { id, src, name, email, busy, active },
+  setCards,
+  cards,
+}) {
+  const [isActive, setIsActive] = useState(active);
+  const [newCard, setNewCard] = useState({});
+  //   console.log(setCards);
+  // Destructuralizing
+  // const {src, name, email} = props;
 
-    // Destructuralizing
-    // const {src, name, email} = props;
-    console.log(src)
+  const handleClick = () => {
+    setIsActive((prev) => !prev);
+  };
 
+  useEffect(() => {
+    let removedArray = cards.filter((card) => card.id !== id);
+    console.log(removedArray);
+    let card = {
+      id,
+      src,
+      name,
+      email,
+      busy,
+      active: isActive,
+    };
+    console.log(card);
+    setCards([...removedArray, card]);
+  }, [isActive]);
+
+  console.log(cards);
   return (
-    <div className='card-container'>
-      <div className='image-container'>
-        <img src={src} alt='profile' />
-      </div>
-      <div className='card-text'>
-        <p>Name: {name}</p>
-        <p>Email:{email}</p>
-      </div>
+    <div className={`card-container ${busy ? 'redBg' : ''}`}>
+      {active && (
+        <div>
+          <div className='image-container'>
+            <img src={src} alt='profile' />
+          </div>
+          <div className='card-text'>
+            <p>Name: {name}</p>
+            <p>Email:{email}</p>
+          </div>
+        </div>
+      )}
+      <button onClick={handleClick}>Activate</button>
     </div>
   );
 }
