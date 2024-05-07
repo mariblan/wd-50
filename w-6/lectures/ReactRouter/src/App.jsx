4c7a4407a8ea79e6a373ff178dcbd811e3a711bb
mariblan <mariablancocanal@gmail.com>
2024-05-07 12:02:42 +0200

    Added dynamic routing for react router lecture

--- w-6/lectures/ReactRouter/src/App.jsx
+++ w-6/lectures/ReactRouter/src/App.jsx
@@ -3,12 +3,28 @@ import { Routes, Route } from 'react-router-dom';
 import Component1 from './components/Component1';
 import Component2 from './components/Component2';
 import Component3 from './components/Component3';
+import DynamicComponent from './components/DynamicComponent';
 import { useState } from 'react';
 import { NavLink, Link, useNavigate } from 'react-router-dom';
 
 function App() {
   const [todos, setTodos] = useState([]);
   const navigate = useNavigate();
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: 'React Router',
+      words: 200,
+      text: 'Lorem ipsum......',
+    },
+    {
+      id: 2,
+      title: 'SQL',
+      words: 150,
+      text: 'Lorem ipsum......',
+    },
+  ]);
+
   const handleClick = () => {
     navigate(-1);
   };
@@ -28,7 +44,14 @@ function App() {
         Page2
       </NavLink>
       <br />
-      <Link to='/page3'>Page3</Link>
+      <NavLink
+        style={({ isActive }) => ({ color: isActive ? 'purple' : 'blue' })}
+        to='/page3'
+      >
+        Page3
+      </NavLink>
+
+      {/* <Link to='/page3'>Page3</Link> */}
 
       <Routes>
         <Route
@@ -36,8 +59,18 @@ function App() {
           element={<Component1 todos={todos} setTodos={setTodos} />}
         />
         <Route path='/page2' element={<Component2 onClick={handleClick} />} />
-        <Route path='/page3' element={<Component3 onClick={handleClick} />} />
-        {/* <Routes></Routes> */}
+        <Route
+          path='/page3'
+          element={<Component3 posts={posts} onClick={handleClick} />}
+        />
+        <Route
+          path='/page3'
+          element={<Component3 posts={posts} onClick={handleClick} />}
+        >
+           <Route path='/page3/:id' element={<DynamicComponent posts={posts} />} />
+        </Route>
+
+        {/* <Route path='/page3/:id' element={<DynamicComponent posts={posts} />} /> */}
       </Routes>
     </div>
   );
--- w-6/lectures/ReactRouter/src/components/Component3.jsx
+++ w-6/lectures/ReactRouter/src/components/Component3.jsx
@@ -1,12 +1,21 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link, Outlet } from 'react-router-dom';
 
-function Component3({ onClick }) {
+function Component3({ posts, onClick }) {
   const navigate = useNavigate();
+  console.log(posts);
   return (
     <div>
       <h2>Component3</h2>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>
+            <Link to={`/page3/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
       <button onClick={onClick}>Go Back</button>
       <button onClick={() => navigate(+1)}>Go Forward</button>
+      <Outlet />
     </div>
   );
 }
